c3bc9d1e3c28c6f4127766e41e5c248da2130219
KosukeY920 <yokomaekatuo@gmail.com>
2019-10-04 13:47:15 +0900

    kadai2 完成

--- kadai2/Simpson_rule.c
+++ kadai2/Simpson_rule.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#define N 32
+#define N 8
 
 double integration_func(double x);
 double simpson_rule(double a, double b);
@@ -21,26 +21,31 @@ double simpson_rule (double a,double b){
 
     double y0 = integration_func(a);
     double Yn = integration_func(b);
-    double tmp_kisu = a;
-    double Y_kisu = 0.0;
-    double kisu_res_tmp = 0.0;
-    double tmp_gusu = a;
-    double Y_gusu = 0.0;
-    double gusu_res_tmp = 0.0;
+    //奇数
+    double tmp_odd = a - h;
+    double Y_odd = 0.0;
+    double odd_res_tmp = 0.0;
+    //偶数
+    double tmp_even = a;
+    double Y_even = 0.0;
+    double even_res_tmp = 0.0;
 
-    for (int i = 1; i < N-1; i += 2){
-        tmp = tmp + h;
-        Yj = integration_func(tmp);
-        res_tmp += Yj;
+    for (int i = 1; i < N; i += 2){
+        tmp_odd = tmp_odd + 2 * h;
+        Y_odd = integration_func(tmp_odd);
+        odd_res_tmp += Y_odd;
     }
 
-    for (int i = 2; i < N - 2; i += 2){
-
+    for (int i = 2; i < N; i += 2){
+        tmp_even = tmp_even + 2 * h;
+        Y_even = integration_func(tmp_even);
+        even_res_tmp += Y_even;
     }
     
-    res_tmp *= 2.0;
+    odd_res_tmp *= 4.0;
+    even_res_tmp *= 2.0;
 
-    double result = (h / 2.0) * (y0 + res_tmp + Yn);
+    double result = (h / 3.0) * (y0 + odd_res_tmp  + even_res_tmp + Yn);
 
     return result;
 }

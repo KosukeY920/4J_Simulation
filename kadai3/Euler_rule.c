d3bdce22934c427806ae2b9a81c49d58bd7ec69e
KosukeY920 <yokomaekatuo@gmail.com>
2019-10-11 09:50:34 +0900

    Add: Euler_rule

---
+++ kadai3/Euler_rule.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+double bibun_func (double x, double y);
+double euler_rule (double x, double h);
+
+int main (void){
+    double* x, y;
+    double h;
+}
+
+double bibun_func (double x, double y){
+    double result = y / 1 + x;
+    return result;
+}
+
+double euler_rule (double *x, double *h){
+    
+}

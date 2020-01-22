f44981fb3e8682fb1700c4b312d685aebb664ab9
KosukeY920 <yokomaekatuo@gmail.com>
2020-01-22 10:55:47 +0900

    Finish:kadai9, Change:kadai7.pdf

--- kadai9/Lorentz_force.c
+++ kadai9/Lorentz_force.c
@@ -1,92 +1,63 @@
 #include <stdio.h>
 #include <math.h>
 
-double diff_equa_1 (double);
+double diff_equa_1 (double, double);
 double diff_equa_2 (double);
-double euler_method (double t, double y, double v, double h, int step);
-double heun_method (double t, double I, double Q,double dt, int step);
+double heun_method (double, double, double, double, int);
 
-double new_t = 0.0;
-double new_y = 0.0;
-double new_v = 0.0;
-
-double R = 1.0;
-double C = 0.3;
-double L = 10.0;
-double Q_0 = 10.0;
-
-double calculated_t = 0.0;
-double calculated_I = 0.0;
+double qB_m = 2.0;
 
 int main (void){
     double t = 0.0;
-    double Q = Q_0;
-    double I = 0.0;
+    double x = 0.0;
+    double v = 1.0;
 
-    double dt = 0.025;
-    int step = 4;
+    double dt = 0.01;
+    int step = 1;
     
-    printf ("i, t, Q, I\n");
-    printf ("0, %f, %f, %f\n", t, Q, I);
-    heun_method(t, I, Q, dt, step);
+    printf ("i, t, x, v\n");
+    printf ("0, %f, %f, %f\n", t, x, v);
+    heun_method(t, x, v, dt, step);
 }
+
 
 //微分方程式
 //式1
-//dv/dt = (qB/m) * v
-//v' = 2 * v
-double diff_equa_1 (double v){
-    double result = 2 * v;
+// v' = (qB / m) * v
+double diff_equa_1 (double x, double v){
+    double result = qB_m * v;
     return result;
 }
 
+
 //微分方程式
 //式2
-//dx/dt = v
-//x' = v
+// x' = v
 double diff_equa_2 (double v){
     double result = v;
     return result;
 }
 
-//オイラーの公式
-/*double euler_method (double t, double y, double v, double h, int step){
+//ホイン法
+double heun_method (double t, double x, double v, double dt, int step){
     double old_t = t;
-    double old_y = y;
+    double old_x = x;
     double old_v = v;
     
     for(int i = 0; i < step; i++){
+        double new_t = old_t + dt;
+        double k1_eq1 = (dt * diff_equa_1(old_x, old_v));
+        double k2_eq1 = (dt * diff_equa_1(old_x + dt, old_v + k1_eq1));
+        double k1_eq2 = (dt * diff_equa_2(old_v));
+        double k2_eq2 = (dt * diff_equa_2(old_v + k1_eq2));
 
-        new_t = old_t + h;
-        new_y = old_y + (h * diff_equa_1(k, l, old_y, old_v));
-        new_v = old_v + (h * diff_equa_2(old_v));
+        double new_v = old_v + (0.5 * (k1_eq1 + k2_eq1));
+        double new_x = old_x + (0.5 * (k1_eq2 + k2_eq2));
         old_t = new_t;
-        old_y = new_y;
         old_v = new_v;
-        printf("%d, %f, %f, %f\n", i + 1, new_t, new_y, new_v);
+        old_x = new_x;
+        printf("%d, %f, %f, %f\n", i + 1, old_t, old_v, old_x);
     }
-
-    return 0;
-}*/
-
-//ホイン法
-double heun_method (double t, double I, double Q, double dt, int step){
-    double t_i = t;
-    double I_i = I;
-    
-
-    for(int i = 0; i < step; i++){
-        double t_i1 = t_i + dt;
-        double k1 = (dt * diff_equa_1(R, Q, I_i, C, L));
-        double k2 = (dt * diff_equa_1(R, Q + dt, I_i + k1, C, L));
-
-        double I_i1 = I_i + (0.5 * (k1 + k2));
-        t_i = t_i1;
-        I_i = I_i1;
-        printf("%d, %f, %f, %f\n", i + 1, new_t, new_y, new_v);
-    }
-    calculated_t = t_i;
-    calculated_I = I_i;
 
     return 0;
 }

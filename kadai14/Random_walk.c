cae4d4184a3e9d8d64be3ff012e0f0f822034710
KosukeY920 <yokomaekatuo@gmail.com>
2020-01-31 12:03:25 +0900

    ADD:課題ファイル, kadai14の近似の一次直線を求める

--- kadai14/Random_walk.c
+++ kadai14/Random_walk.c
@@ -1,45 +1,77 @@
 #include <sys/time.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
-#define STEP 100
+
+//#define STEP 500
 #define SAMPLE_CNT 10
 
 int main(void){
-    double ave_xN;
-    double pow2_xN;
+    double ave_xN = 0;
+    double pow2_xN = 0;
     double bunsan = 0;
-    for(int k = 0; k < 10; k++){
-    for(int j = 0; j < SAMPLE_CNT; j++){
-        int xN = 0;
+    double sum_xy = 0, sum_x = 0, sum_y = 0, sum_x_pow2 = 0;
+    double a = 0, b = 0;
+    const int STEP = 500;
 
-        for(int i = 0; i < STEP; i++){    
-            struct timeval miri;
-            gettimeofday(&miri, NULL);
-            srand(miri.tv_usec);
-            int a = rand() % 2;
-            if(a == 1){
-                xN++;
-            }else{
-                xN--;
+    struct timeval miri;
+    gettimeofday(&miri, NULL);
+    srand((unsigned int)time(NULL));
+
+    printf("STEP, bunsan\n");
+    for(int N = 1; N <= 10; N++){
+
+    
+        for(int k = 0; k < 10; k++){
+            double sum_xN = 0, sum_pow2_xN = 0;
+            for(int j = 0; j < SAMPLE_CNT; j++){
+                int xN = 0;
+
+                for(int i = 0; i < STEP * N; i++){    
+            
+                    int a = rand() % 2;
+                    if(a == 1){
+                        xN++;
+                    }else{
+                        xN--;
+                    }
+                }
+
+                //位置の和
+                sum_xN += xN;
+                //位置の2乗の和
+                sum_pow2_xN += xN * xN;
+
+                
+                
             }
-            
-            
+            //位置の平均
+            ave_xN += sum_xN / SAMPLE_CNT;
+            //位置の2乗の平均
+            pow2_xN += sum_pow2_xN / SAMPLE_CNT;
+    
+    
+            //分散の和
+            bunsan += (pow2_xN - pow(ave_xN, 2.0));
         }
-        //printf("%d\n", xN);
-
-        ave_xN += xN;
-        pow2_xN += xN * xN;
-    }
-
-    ave_xN /= STEP;
-    pow2_xN /= STEP;
-
-    bunsan += pow2_xN - pow(ave_xN, 2.0);
-    }
 
 //    printf("ave_x(N):%f\n", ave_xN);
-    bunsan /= STEP;
-    printf("bunsan:%f\n", bunsan);
+        //分散の平均
+        bunsan /= SAMPLE_CNT;
+        printf("%d, %f\n", STEP * N, bunsan);
+
+        sum_xy += STEP * N * bunsan;
+        sum_x += STEP * N;
+        sum_y += bunsan;
+        sum_x_pow2 += pow(STEP * N, 2);
+    }
+
+    a = (10 * sum_xy - sum_x * sum_y) / (10 * sum_x_pow2 - pow(sum_x, 2));
+    b = (sum_x_pow2 * sum_y - sum_xy * sum_x) / (10 * sum_x_pow2 - pow(sum_x, 2));
+
+    printf("a = %f\nb = %f\n", a, b);
+
     return 0;
 }
